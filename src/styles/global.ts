511718c926e857a1300a2f2dc451814f3fab689f
Patricia Faustino <patriciafaustinobr@gmail.com>
2022-05-23 23:03:24 -0300

    creating application structure

---
+++ src/styles/global.ts
@@ -0,0 +1,60 @@
+import {createGlobalStyle} from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+    :root {
+        --red: #E52E4D;
+        --blue: #5429CC;
+
+        --blue-light: #6933FF;
+
+        --text-title: #363F5F;
+        --text-body: #969CB3;
+
+        --background: #F0F2F5;
+        --shape: #FFFFFF;
+    }
+
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    //padrão --> font-size: 16px (Desktop)
+    // acessibilidade
+    // aumentar/diminuir de acordo com a tela
+    html {
+        @media (max-width: 1088px) {
+            font-size: 93,75%; //15px
+        }
+
+        @media (max-width: 720px) {
+            font-size: 87.5%; //14px
+        }
+    }
+
+    //REM = 1rem = 16px
+
+    body {
+        background: var(--background);
+        -webkit-font-smoothing: antialiased;
+    }
+
+    body, input, textarea, button {
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400;
+    }
+
+    h1, h2, h3, h4, h5, h6, strong {
+        font-weight: 600;
+    }
+
+    button {
+        cursor: pointer;
+    }
+
+    [disabled] {
+        opacity: 0.6;
+        cursor: not-allowed;
+    }
+`
